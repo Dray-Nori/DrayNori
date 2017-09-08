b896c9e3712c28231f12d6e20c8fe20c8a9ca0f5
OrestiArsi <orestiarsi@gmail.com>
2017-09-08 12:22:41 -0400

    added CRUD to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,30 @@
-class V1::PagesController < ApplicationController
+class PagesController < ApplicationController
 
  def index
   @pages = Page.all
   # p @stories
-  render json: @pages
+  render json: @pages, status: :ok
  end
+
+ def create
+  @page = Page.new(page_params)
+
+  @page.save
+  render json: @page, status: :ok
+ end
+
+ def destroy
+  @page = Page.where(id: params[:id]).first
+  if @page.destroy
+   head(:ok)
+  else
+   head(:unprocessable_entity)
+  end
+ end
+
+   private
+
+   def page_params
+    params.require(:page).permit(:name, :img, :text, :story_id)
+   end
 end
