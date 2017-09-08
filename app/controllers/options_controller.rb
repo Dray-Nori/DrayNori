70264839e3ea4a9bf853c807a0ac6602bab5c1ab
OrestiArsi <orestiarsi@gmail.com>
2017-09-08 12:15:30 -0400

    added CRUD to options controller

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,2 +1,28 @@
-class V1::OptionsController < ApplicationController
+class OptionsController < ApplicationController
+ def index
+  @options = Option.all
+  render json: @options, status: :ok
+ end
+
+ def create
+  @option = Option.new(option_params)
+
+  @option.save
+  render json: @option, status: :ok
+ end
+
+ def destroy
+  @option = Option.where(id: params[:id]).first
+  if @option.destroy
+   head(:ok)
+  else
+   head(:unprocessable_entity)
+  end
+ end
+
+    private
+
+    def option_params
+     params.require(:option).permit(:text, :change, :page_id, :next_page_id)
+    end
 end
