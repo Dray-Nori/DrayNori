183e7f2bd92c718719a66c0dcdd6423507cb32d6
OrestiArsi <orestiarsi@gmail.com>
2017-09-08 12:28:28 -0400

    added CRUD to stories controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,8 +1,31 @@
-class V1::StoriesController < ApplicationController
+class StoriesController < ApplicationController
 
  def index
   @stories = Story.all
   # p @stories
-  render json: @stories
+  render json: @stories, status: :ok
  end
+
+ def create
+  @stories = Story.new(story_params)
+
+  @storty.save
+  render json: @story, status: :ok
+ end
+
+ def destroy
+  @story = Story.where(id: params[:id]).first
+  if @story.destroy
+   head(:ok)
+  else
+   head(:unprocessable_entity)
+  end
+ end
+
+    private
+
+    def story_params
+     params_require(:story).permit(:name, :img, :language_id)
+    end
+
 end
